a6fe4aca9ec7f3ef4694ef8f74092a0b1bd0766d
Maik Schneider <m.schneider@blueways.de>
2022-02-23 12:10:38 +0100

    feat: initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Todo App',
+    'description' => 'move:elevator application test',
+    'category' => 'templates',
+    'author' => 'Maik Schneider',
+    'author_mail' => 'schneider.maik@me.com',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => '1',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.7-11.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
