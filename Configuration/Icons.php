7b5b9aab0db8cd1ad8feafbe7afa4047c9041414
Maik Schneider <m.schneider@blueways.de>
2022-03-02 03:40:51 +0100

    feat: use new icon registration

---
+++ Configuration/Icons.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'apps-pagetree-folder-todo' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bw_todo/Resources/Public/Images/Icons/apps-pagetree-folder-todo.svg'
+    ],
+    'profile' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bw_todo/Resources/Public/Images/Icons/profile.svg'
+    ],
+    'task' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bw_todo/Resources/Public/Images/Icons/task.svg'
+    ]
+];
