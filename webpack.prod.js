ec0065b4aa5d10c442f56c570ed7a25a7c59b534
Maik Schneider <m.schneider@blueways.de>
2022-03-02 03:27:02 +0100

    feat: new logo

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,20 @@
 const {merge} = require('webpack-merge');
 const common = require('./webpack.config.js');
 const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: false,
-  watch: false,
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin({extractComments: false})],
-  },
+    mode: 'production',
+    devtool: false,
+    watch: false,
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({extractComments: true})],
+    },
+    output: {
+        filename: 'Resources/Public/JavaScript/dist/[name].min.js',
+    },
+    plugins: [new MiniCssExtractPlugin({
+        filename: 'Resources/Public/Css/dist/[name].min.css',
+    })],
 });
