f57aea122c20ce0cbd5763276016877c5d9fe5a3
Maik Schneider <m.schneider@blueways.de>
2022-05-31 02:06:21 +0200

    feat: add deploy.php

---
+++ deploy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Deployer;
+
+require_once(__DIR__ . '/vendor/blueways/deployer-recipes/autoload.php');
+
+set('repository', 'git@github.com:maikschneider/bw_todo.git');
+
+host('staging')
+    ->hostname('maik-tailor.de')
+    ->stage('staging')
+    ->user('web0')
+    ->port('24327')
+    ->set('branch', 'master-deployer')
+    ->set('public_urls', ['https://deployer.maik-tailor.de'])
+    ->set('http_user', 'www-data')
+    ->set('writable_mode', 'chmod')
+    ->set('bin/composer', '/usr/local/bin/composer')
+    ->set('bin/php', '/usr/bin/php7.4')
+    ->set('deploy_path', '/home/web0/vhosts/deployer.maik-tailor.de/htdocs/branches/master');
+
+host('local')
+    ->hostname('local')
+    ->set('deploy_path', getcwd());
